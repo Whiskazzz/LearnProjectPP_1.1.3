12085d30c721e9da5b3ef4a5dd99accb3dde745b
Whiskazzz <wilker2010@yandex.ru>
2022-05-17 16:19:14 +0300

    1.0

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,19 @@
+package jm.task.core.jdbc.util;
+
+
+import java.sql.*;
+
+public class Util {
+    public static final String URL = "jdbc:mysql://localhost:3306/pp";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "***";
+    private static Connection connection;
+    private static Statement statement;
+    public static Statement getStatement() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            statement = connection.createStatement();
+        }
+        return statement;
+    }
+}
