f7868ce0bd6cc2c8450653a4fbbd0c3a74c03a75
Whiskazzz <wilker2010@yandex.ru>
2022-05-18 11:04:43 +0300

    1.1

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,7 @@ import java.sql.*;
 public class Util {
     public static final String URL = "jdbc:mysql://localhost:3306/pp";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "***";
+    public static final String PASSWORD = "***1";
     private static Connection connection;
     public static Connection getConnection() throws SQLException {
         if (connection == null) {
